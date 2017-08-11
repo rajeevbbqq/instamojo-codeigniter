b9f0336e0bda2c7c97e8bc0166ec45514a4b2029
Rajeev <scraptorajeev@gmail.com>
2017-08-11 11:36:41 +0530

    Update instamojo.php

--- config/instamojo.php
+++ config/instamojo.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 *
 * @license Creative Commons Attribution 3.0 <http://creativecommons.org/licenses/by/3.0/>
 * @version 1.0
-* @author Rajeev bbqq <http://bbqq.me>
+* @author Rajeev bbqq <https://github.com/rajeevbbqq>
 * @copyright Copyright (c) 2017, Rajeev bbqq
 */
 
